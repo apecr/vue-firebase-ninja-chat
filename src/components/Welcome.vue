9129a85274cbf31dd94842061e37fbfd0a39e488
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-11-02 12:48:30 +0100

    feat() New Chat component and push from Welcome component
    [FA-139]

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,6 +6,7 @@
       <form @submit.prevent="enterChat">
         <label for="name">Enter your name:</label>
         <input type="text" name="name" v-model="name">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn teal">Enter Chat</button>
       </form>
     </div>
@@ -18,12 +19,17 @@ export default {
   name: "Welcome",
   data() {
     return {
-      name: null
+      name: null,
+      feedback: null
     };
   },
   methods: {
     enterChat() {
-      console.log(this.name);
+      if (!this.name) {
+        return (this.feedback = "You must enter a valid name to join");
+      }
+      this.feedback = null;
+      this.$router.push({ name: "Chat", params: { name: this.name } });
     }
   }
 };
